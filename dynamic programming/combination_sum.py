61a5ff32b628a0044cbb2f7130b4e6e0257668e4
Aakarsh Sharma <71917139+aakarsh27@users.noreply.github.com>
2024-09-14 13:58:31 +0530

    Add files via upload

---
+++ dynamic programming/combination_sum.py
@@ -0,0 +1,16 @@
+def combinationSum(candidates, target):
+       
+        dp = [[] for _ in range(target + 1)]
+        dp[0].append([])
+
+        for i in range(1, target + 1):
+            for candidate in candidates:
+                if candidate <= i:
+                    for prev in dp[i - candidate]:
+                        temp = prev + [candidate]
+                        temp.sort()
+                        if temp not in dp[i]:
+                            dp[i].append(temp)
+        return dp[target]
+        
+print(combinationSum([2,3,6,7], 7))
