37e1bb6a06d92ad67750f24461ed0036d14057a7
Aakarsh Sharma <71917139+aakarsh27@users.noreply.github.com>
2024-09-14 13:44:28 +0530

    Add files via upload

---
+++ dynamic programming/word_break.py
@@ -0,0 +1,13 @@
+def wordBreak(s, wordDict):
+        dp = [True] + [False] * len(s)
+
+        for i in range(1, len(s) + 1):
+            for w in wordDict:
+                start = i - len(w)
+                if start >= 0 and dp[start] and s[start:i] == w:
+                    dp[i] = True
+                    break
+        
+        return dp[-1]
+        
+print(wordBreak("leetcode", ["leet","code"]))
